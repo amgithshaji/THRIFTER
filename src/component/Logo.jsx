38e30e6f2da9fd6033ef090e8f15c522e815df0d
amgithshaji <amgithshaji410@gmail.com>
2025-12-21 18:04:24 +0530

    landing completed almost

---
+++ src/component/Logo.jsx
@@ -0,0 +1,56 @@
+import LogoLoop from "@/components/LogoLoop"
+import { SiReact, SiNextdotjs, SiTypescript, SiTailwindcss } from 'react-icons/si';
+
+import React from 'react'
+const techLogos = [
+  { node: <SiReact />, title: "React", href: "https://react.dev" },
+  { node: <SiNextdotjs />, title: "Next.js", href: "https://nextjs.org" },
+  { node: <SiTypescript />, title: "TypeScript", href: "https://www.typescriptlang.org" },
+  { node: <SiTailwindcss />, title: "Tailwind CSS", href: "https://tailwindcss.com" },
+];
+
+// Alternative with image sources
+const imageLogos = [
+  { src: "/logoloop1.png", alt: "Company 1", href: "https://company1.com" },
+  { src: "/logoloop2.png", alt: "Company 1", href: "https://company1.com" },
+  { src: "/logoloop3.png", alt: "Company 1", href: "https://company1.com" },
+  { src: "/logoloop4.png", alt: "Company 1", href: "https://company1.com" },
+  { src: "/logoloop5.png", alt: "Company 1", href: "https://company1.com" },
+
+
+];
+
+function Logo() {
+  return (
+    <div>
+        <div style={{ height: '200px', position: 'relative', overflow: 'hidden'}}>
+      {/* Basic horizontal loop */}
+      <LogoLoop
+        logos={techLogos}
+        speed={120}
+        direction="left"
+        logoHeight={45}
+        gap={80}
+        hoverSpeed={0}
+        scaleOnHover
+        fadeOut
+        fadeOutColor="#ffffff"
+        ariaLabel="Technology partners"
+      />
+      
+      {/* Vertical loop with deceleration on hover */}
+      {/* <LogoLoop
+        logos={techLogos}
+        speed={80}
+        direction="up"
+        logoHeight={48}
+        gap={40}
+        hoverSpeed={20}
+        fadeOut
+      /> */}
+    </div>
+    </div>
+  )
+}
+
+export default Logo
--- src/users/pages/Home.jsx
+++ src/users/pages/Home.jsx
@@ -1,97 +1,244 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import TextPressure from "@/components/TextPressure"
 import ShinyText from "@/components/ShinyText"
 import StaggeredMenu from '@/components/StaggeredMenu'
+import { GoMute, GoUnmute } from "react-icons/go";
+import SplitText from "@/components/SplitText"
+import StarBorder from "@/components/StarBorder"
+import Logo from '@/component/Logo';
+import ScrollStack, { ScrollStackItem } from '@/components/ScrollStack';
+import Lenis from "lenis";
 
 function Home() {
+
+  const videoRef = useRef();
+  const [muted, setMuted] = useState(true);
+  // split text
+  const handleAnimationComplete = () => {
+    console.log('All letters have animated!');
+  };
+
   const menuItems = [
-  { label: 'Home', ariaLabel: 'Go to home page', link: '/' },
-  { label: 'About', ariaLabel: 'Learn about us', link: '/about' },
-  { label: 'Services', ariaLabel: 'View our services', link: '/services' },
-  { label: 'Login', ariaLabel: 'Get in touch', link: '/contact' }
-];
+    { label: 'Home', ariaLabel: 'Go to home page', link: '/' },
+    { label: 'About', ariaLabel: 'Learn about us', link: '/about' },
+    { label: 'Services', ariaLabel: 'View our services', link: '/services' },
+    { label: 'Login', ariaLabel: 'Get in touch', link: '/contact' }
+  ];
 
-const socialItems = [
-  { label: 'Twitter', link: 'https://twitter.com' },
-  { label: 'GitHub', link: 'https://github.com' },
-  { label: 'LinkedIn', link: 'https://linkedin.com' }
-];
+  const socialItems = [
+    { label: 'Twitter', link: 'https://twitter.com' },
+    { label: 'GitHub', link: 'https://github.com' },
+    { label: 'LinkedIn', link: 'https://linkedin.com' }
+  ];
+  const toggleMute = () => {
+    const video = videoRef.current;
+    // if (!video) return;
 
+    video.muted = !muted;
+    setMuted(!muted);
+  };
   return (
 
-<div className='h-auto'>
+    <div className=''>
 
-       <StaggeredMenu
-      position="fixed"
-      items={menuItems}
-      socialItems={socialItems}
-      isFixed={true}
-      displaySocials={true}
-      displayItemNumbering={true}
-      menuButtonColor="#000"
-      openMenuButtonColor="#fff"
-      changeMenuColorOnOpen={true}
-      colors={['#B19EEF', '#5227FF']}
-      logoUrl="./logo2.png"
-      accentColor="#ff6b6b"
-      onMenuOpen={() => console.log('Menu opened')}
-      onMenuClose={() => console.log('Menu closed')}
-    />
+      <StaggeredMenu
+        position="fixed"
+        items={menuItems}
+        socialItems={socialItems}
+        isFixed={true}
+        displaySocials={true}
+        displayItemNumbering={true}
+        menuButtonColor="#000"
+        openMenuButtonColor="#fff"
+        changeMenuColorOnOpen={true}
+        colors={['#B19EEF', '#5227FF']}
+        logoUrl="./logo2.png"
+        accentColor="#ff6b6b"
+        onMenuOpen={() => console.log('Menu opened')}
+        onMenuClose={() => console.log('Menu closed')}
+      />
 
 
-  <div className='grid md:grid-cols-2 grid-cols-1 ' >
-       <div className=" flex items-center justify-center flex-col  md:mt-60 mt-30  ">
-        <TextPressure 
-        text="THRIFTER"
-        textColor='black' 
-          fontSize={54}
+      <div className='grid md:grid-cols-2 grid-cols-1 h-screen' >
+        <div className=" flex items-center justify-center flex-col  md:mt-60 mt-30  ">
+          <TextPressure
+            text="THRIFTER"
+            textColor='black'
+            fontSize={54}
             minfontWeight={800}
             minFontSize={100}
-        />
-  <div>
-    
-  
-  <div style={{fontFamily:"Playfair Display, serif"}} className=' md:mb-5 mb-5 md:mt-1 mt-2 md:me-60 me-35 italic '>
-     <div className='md:text-4xl text-base font-bold'>
-        <ShinyText 
-          text="Welcome to Thrifter" 
-          speed={3} 
-          className='custom-class' 
-        />
-     </div>
-     <div className=' italic md:text-2xl text-xs font-medium '>
-        <ShinyText 
-          text="This is your premium fashion destination." 
-          speed={3} 
-          className='custom-class' 
-        />
-     </div>
-    
- 
-  </div>
-  
-  
-    
-  </div>
+          />
+          <div>
+
+
+            <div style={{ fontFamily: "Playfair Display, serif" }} className=' md:mb-5 mb-5 md:mt-1 mt-2 md:me-60 me-35 italic '>
+              <div className='md:text-4xl text-base font-bold'>
+                <ShinyText
+                  text="Welcome to Thrifter"
+                  speed={3}
+                  className='custom-class'
+                />
+              </div>
+              <div className=' italic md:text-2xl text-xs font-medium '>
+                <ShinyText
+                  text="This is your premium fashion destination."
+                  speed={3}
+                  className='custom-class'
+                />
+              </div>
+
+
+            </div>
+
+
+
+          </div>
+        </div>
+
+        <div>
+          <div className='md:ms-20 ms-4 relative group'>
+            <img className=' me-6  md:h-150 h-80 md:w-160 w-89 object-cover object-top relative ' src="https://static.zara.net/assets/public/63fd/6d79/8d8e46339eb5/ab97b76b2478/00858613250-p/00858613250-p.jpg?ts=1765983610422&w=1024" alt="no img" />
+            <button style={{ fontFamily: "Playfair Display, serif" }} className=' md:text-sm text-xs absolute md:left-124 left-57  md:top-139 top-71 -translate-x-1/9 -translate-y-1/9 border border-black text-black cursor-pointer px-6 py-1 md:hover:border-yellow-50 hover:border-yellow-50 md:hover:text-yellow-50 hover:text-yellow-50  active:border-white active:text-white   transition-all duration-300 ease-out' >View product</button>
+            <div className='md:mt-13 mt-4 flex  '>
+              <p style={{ fontFamily: 'Raleway, sans-serif' }} className='md:text-base text-xs italic'>Founder:<br />RETRO PREPPY</p>
+              <p style={{ fontFamily: "Playfair Display, serif" }} className='md:me-10 me-5 md:text-2xl text-xs italic'> one of everything<br />really good</p>
+            </div>
+          </div>
+
+        </div>
       </div>
-    
+      {/* bg video */}
+      <div className="relative h-screen w-full overflow-hidden">
+
+        <video className="absolute inset-0 w-full h-full object-cover" ref={videoRef}
+          autoPlay muted loop playsInline>
+          <source src="/bgvideo.mp4" type="video/mp4" />
+        </video>
+
+        {/* Optional overlay */}
+        {/* <div className="absolute inset-0 bg-black/50 z-10"></div> */}
+
+        {/* Content */}
+        <div className="relative z-20  ">
+          <h1 style={{ fontFamily: "Playfair Display, serif" }} className="text-white  tracking-widest">
+
+            <SplitText
+              text="Thrifter"
+              className="md:text-[400px] text-[100px] font-bold"
+              delay={100}
+              duration={0.9}
+              ease="power3.out"
+              splitType="chars"
+              from={{ opacity: 0, y: 40 }}
+              to={{ opacity: 1, y: 0 }}
+              threshold={0.1}
+              rootMargin="-100px"
+              textAlign="center"
+              onLetterAnimationComplete={handleAnimationComplete}
+            />
+            <div className='flex flex-col md:mt-1 mt-145  '>
+              <ShinyText
+
+                text="Not Your Ordinary"
+                speed={5}
+                className='custom-class md:text-3xl text-2xl md:ms-40 ms-10'
+              />
+              <ShinyText
+
+                text=" Clothing Store."
+                speed={5}
+                className='custom-class md:text-3xl text-2xl md:ms-30 ms-5'
+              />
+              <ShinyText
+
+                text="- Thrifter"
+                speed={5}
+                className='custom-class md:text-1xl text-base md:ms-75 ms-42 italic md:mt-5 mt-3'
+              />
+            </div>
+          </h1>
+        </div>
+        {/* Mute / Unmute Button */}
+        <button onClick={toggleMute} className="absolute z-30 bottom-6 right-6 ">
+          {muted ? < GoMute size={19} /> : <GoUnmute size={19} />}
+        </button>
+      </div>
       <div>
-  <div className='md:ms-20 ms-4 relative group'>
-          <img className=' me-6  md:h-150 h-80 md:w-160 w-89 object-cover object-top relative '  src="https://i.pinimg.com/1200x/bc/63/dd/bc63dde14b5ff98b314601718e12e4ec.jpg" alt="no img" />
-          <button  style={{fontFamily:"Playfair Display, serif"}}  className=' md:text-sm text-xs absolute md:left-124 left-57  md:top-139 top-71 -translate-x-1/9 -translate-y-1/9 border border-black text-black cursor-pointer px-6 py-1 md:hover:border-white hover:border-white md:hover:text-white hover:text-white  active:border-white active:text-white  '   transition-all duration-300 ease-out >View product</button>
-    <div className='md:mt-13 mt-4 flex  '>
-    <p style={{  fontFamily: 'Raleway, sans-serif'}} className='md:text-base text-xs italic'>Founder:<br />RETRO PREPPY</p>
-    <p style={{fontFamily:"Playfair Display, serif"}} className='md:me-10 me-5 md:text-2xl text-xs italic'> one of everything<br />really good</p>
-  </div>  
-  </div>  
-  
+        <div className='text-center md:mt-10 mt-7 ' >
+          <h1 style={{ fontFamily: "Playfair Display, serif" }}  >
+        <SplitText
+              text="New Drop"
+              className="md:text-6xl text-3xl"
+              delay={100}
+              duration={0.9}
+              ease="power3.out"
+              splitType="chars"
+              from={{ opacity: 0, y: 40 }}
+              to={{ opacity: 1, y: 0 }}
+              threshold={0.1}
+              rootMargin="-100px"
+              textAlign="center"
+              onLetterAnimationComplete={handleAnimationComplete}
+            />
+
+          </h1>
+        </div>
+   <div style={{ fontFamily: "Playfair Display, serif" }}  className='text-center ' >
+      <img
+        className=" md:mt-20 mt-5 md:ms-100 ms-14 md:h-95  h-40 md:w-180 w-70  object-center object-cover"
+        src="https://i.pinimg.com/736x/fc/b8/f7/fcb8f7c246d81ad733ecb907dd52a344.jpg"
+        alt="no img"
+      />
+<StarBorder
+  as="button"
+  className="custom-class mt-3 "
+  color="red"
+  speed="5s"
+>
+  Discover the Selection
+</StarBorder>
+
+   </div>
+   <div className='md:mt-20 mt-5'>
+      <div style={{ fontFamily: "Playfair Display, serif" }} className=' md:mb-20 mb-5 md:mt-1 mt-2 text-center '>
+              <div className='md:text-4xl text-base font-bold'>
+                <ShinyText
+                  text="Brand that collaborate with us"
+                  speed={3}
+                  className='custom-class'
+                />
+              </div>
+              <div className=' italic md:text-2xl text-xs font-medium '>
+                <ShinyText
+                  text="The premium fashion destination for genz"
+                  speed={3}
+                  className='custom-class'
+                />
+              </div>
+
+
+            </div>
+<Logo/>
+   </div>
   </div>
-  </div>
-  
-  
-      
-  
+<div>
+  <ScrollStack>
+  <ScrollStackItem>
+    <h2>Card 1</h2>
+    <p>This is the first card in the stack</p>
+  </ScrollStackItem>
+  <ScrollStackItem>
+    <h2>Card 2</h2>
+    <p>This is the second card in the stack</p>
+  </ScrollStackItem>
+  <ScrollStackItem>
+    <h2>Card 3</h2>
+    <p>This is the third card in the stack</p>
+  </ScrollStackItem>
+</ScrollStack>
+
 </div>
+      </div>
 
   )
 }
